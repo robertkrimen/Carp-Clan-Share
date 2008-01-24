c95261476fec487087d1fd7ac13ffbd5ee479c0a
rob <rob@99b803df-3bca-dc11-90db-000c6e5764c0>
2008-01-24 05:19:08 +0000

    ''

---
+++ lib/Carp/Clan/Share.pm
@@ -0,0 +1,107 @@
+package Carp::Clan::Share;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Carp::Clan::Share - The great new Carp::Clan::Share!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Carp::Clan::Share;
+
+    my $foo = Carp::Clan::Share->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Robert Krimen, C<< <rkrimen at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-carp-clan-share at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Carp-Clan-Share>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Carp::Clan::Share
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Carp-Clan-Share>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Carp-Clan-Share>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Carp-Clan-Share>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Carp-Clan-Share>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 Robert Krimen, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of Carp::Clan::Share
